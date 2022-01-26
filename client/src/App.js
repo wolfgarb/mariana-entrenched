5b0db381ac9839744f22415f849a4648a3d9836b
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2022-01-26 15:12:49 -0500

    setup Apollo imports in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink
+} from '@apollo/client';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
